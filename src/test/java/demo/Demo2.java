0f8ce9a1c067d030d97f929ce016fd80b633d314
siimmii <simi.simmi7@gmail.com>
2023-04-28 08:30:01 +0530

    commit and push

--- src/test/java/demo/Demo2.java
+++ src/test/java/demo/Demo2.java
@@ -5,6 +5,7 @@ public class Demo2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hi demo 2");
+System.out.println("second time trying");
 	}
 
 }
