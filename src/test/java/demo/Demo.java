efe446f23600b4e4a2de3ab0887c43048952da08
siimmii <simi.simmi7@gmail.com>
2023-05-01 08:13:06 +0530

    second commit from seocnd work space

--- src/test/java/demo/Demo.java
+++ src/test/java/demo/Demo.java
@@ -3,14 +3,13 @@ package demo;
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("hi this is my first comment");
-<<<<<<< HEAD
+
 		System.out.println("hi from second work space");
-=======
+
 
 		System.out.println("hi from original work space");
 
 		System.out.println("hi");
->>>>>>> branch 'master' of https://github.com/siimmii/QCO-SJEADD-M1-Demo.git
-	}
+}
 
 }
